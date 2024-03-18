67b9b16eceaf00fa2d71c81225285337ad0e86d9
Anthony Desorbais <anthony@derniercri.io>
2024-03-18 18:04:33 +0100

    Add Express - Middleware and routing concepts

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes);
+const path = require('path');
 
-server.listen(3000);
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
