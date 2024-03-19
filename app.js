2e1e89cc3dcf957bc80cf74e2dcd647c4750d8f0
Anthony Desorbais <anthony@derniercri.io>
2024-03-19 18:15:21 +0100

    Add templating engines, pug first

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
+/**
+ * index.js
+ *
+ * This is the main entry point of our application, this file starts
+ * the Express server and sets up the routes and middleware.
+ */
+
 const express = require('express');
 
 const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+// View engine setup
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+// Middleware Setup
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+// Routes
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
+// 404 Handler
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  // Render the 404 page
+  res.status(404).render('404', { docTitle: 'Page Not Found' });
 });
 
-app.listen(3000);
+// Start the server
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
