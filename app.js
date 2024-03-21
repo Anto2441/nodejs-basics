e86db7adecfb63582edbab0381c51991ff6f99a4
Anthony Desorbais <anthony@derniercri.io>
2024-03-21 23:17:58 +0100

    Add controllers

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const express = require('express');
 
 const app = express();
 
+const errorController = require('./controllers/error');
+
 // This sets the view engine, so that when we call res.render('some-view'),
 // Express knows which template engine to use to render that view.
 // In this case, we're using the EJS view engine.
@@ -20,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // This line imports the routes for the admin section of the site
 // and gives us access to them at `adminData.routes`.
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 
 // This line imports the routes for the shop section of the site
 // and gives us access to them at `shopRoutes`.
@@ -32,14 +34,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 // 404 Handler
-app.use((req, res, next) => {
-  // Render the 404 page
-  res.status(404).render('404', { docTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 // Start the server
 app.listen(3000, () => {
---
+++ controllers/error.js
@@ -0,0 +1,4 @@
+exports.get404 = (req, res, next) => {
+  // Render the 404 page
+  res.status(404).render('404', { docTitle: 'Page Not Found' });
+};
