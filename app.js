986c6ba7832083bd039880d0fe86aee20762b6c7
Anthony Desorbais <anthony@derniercri.io>
2024-06-14 11:55:30 +0200

    feat: errors and validations

--- app.js
+++ app.js
@@ -65,10 +65,15 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then((user) => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      next(new Error(err));
+    });
 });
 
 app.use((req, res, next) => {
@@ -84,6 +89,13 @@ app.use(authRoutes);
 
 // 404 Handler
 app.use(errorController.get404);
+
+// Error Handler
+app.use(errorController.get500);
+
+app.use((error, req, res, next) => {
+  res.redirect('/500');
+});
 
 mongooseConnect(() => {
   app.listen(3000);
